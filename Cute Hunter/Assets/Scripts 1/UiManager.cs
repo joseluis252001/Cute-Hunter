bcc12bafafc9954eb5d9d080c935b969d6cc5305
Jose Luis <joseluis252001@hotmail.com>
2026-03-17 17:16:28 -0600

    prefabs
    creacion de nuevos prefabs y cambio de codifo

--- Cute Hunter/Assets/Scripts 1/UiManager.cs
+++ Cute Hunter/Assets/Scripts 1/UiManager.cs
@@ -1,21 +1,47 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using NaughtyAttributes;
 
 public class UiManager : MonoBehaviour
 {
      [SerializeField] private List <UiWindow> uiWindows = new List<UiWindow>();
 
-
+    private void Initielize()
+    {
+        
+    }
+    [Button]
+    private void FindUiInScene()
+    {
+        //UiWindow.Clear;
+    }
+    public void ShowPopUpUi()
+    {
+        SwowWindow("popupui");
+    }
+    
     public void SwowWindow(string windowId)
     {
+        UiWindow windowToShow = null;
+
         //buscar la ventana, muestrala
         foreach(UiWindow ui in uiWindows)
         {
             if(ui.WindowId == windowId)
             {
-                
-                return;
+                windowToShow = ui;
+                break;
             }
+        }
+
+        if (windowToShow != null)
+        {
+            windowToShow.Show();
+        }
+        else
+        {
+            Debug.LogError($"No se encontro {windowId}" );
         }
     }
 }
